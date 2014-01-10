81f70fb0f02b57b87e2ccd995d90bc4c9dc487d6
mosu <mosu@mosu-PC.lan>
2014-01-10 12:52:56 +0800

    evaluation

---
+++ src/SocExperiment/BFStables.java
@@ -0,0 +1,19 @@
+package SocExperiment;
+
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.Set;
+
+public class BFStables implements Serializable {
+
+	Hashtable<Integer, Hashtable<Integer, Set<Integer>>> Table = new Hashtable<Integer, Hashtable<Integer, Set<Integer>>>();
+	public void addElement()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/SocExperiment/Evaluation.java
+++ src/SocExperiment/Evaluation.java
@@ -1,13 +1,25 @@
 package SocExperiment;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Evaluation {
+public class Evaluation{
 
+	/*public void serializeHash()
+	{
+		MyClass object1 = new MyClass("Hello", 32139604, 2.7e10);
+        //System.out.println("Object 1: " + object1);
+        FileOutputStream fos = new FileOutputStream("hashSerializable/");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(object1);
+        oos.flush();
+        oos.close();
+	}*/
+	
 	public static void main(String[] args) throws IOException {
-		int influenceTargetID = 59263; //default target
-		int MonteCarloTimes = 10;
+		int influenceTargetID = 33043; //default target
+		int MonteCarloTimes = 1000;
 		String network = "com-dblp.ungraph.txt" , propnetwork = "prop-O.txt"; //default data
 		int k = 10; //default
 		
@@ -31,11 +43,16 @@ public class Evaluation {
 		
 		ArrayList<Integer> seeds = new ArrayList<Integer>();
 		seeds.clear();
-		seeds.add(101215);
-		seeds.add(120044);
-		seeds.add(33971);
-		seeds.add(33043);
-		seeds.add(411025);
+		seeds.add(0);
+		//seeds.add(126556);
+		//seeds.add(154258);
+		//seeds.add(118667);
+		//seeds.add(215179);
+		//seeds.add(101215);
+		//seeds.add(120044);
+		//seeds.add(33971);
+		//seeds.add(33043);
+		//seeds.add(411025);
 		//seeds.add(413808);
 		//seeds.add(274042);
 		//seeds.add(1);
@@ -43,7 +60,7 @@ public class Evaluation {
 		/**/
 		d.setSeed(seeds);  //set our seed result 
 		System.out.println("---Evaluation---\nExpected Times: ");
-		System.out.println( d.MC_times(20,influenceTargetID));
+		System.out.println( d.MC_times(MonteCarloTimes,influenceTargetID));
 		
 		endTime = System.currentTimeMillis();
 		totalTime = endTime - startTime;
